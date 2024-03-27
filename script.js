74784ced87eafc32e0d54dc6cebc8adbfa051bfb
gustavo-kley <163368587+gustavo-kley@users.noreply.github.com>
2024-03-27 20:50:19 -0300

    subindo arquivos meu site

---
+++ script.js
@@ -0,0 +1,19 @@
+// function exibirAlerta() {
+//     setTimeout(function() {
+//       var alerta = document.createElement("div");
+//       alerta.classList.add("alerta");
+//       alerta.innerHTML = "Bem-vindo ao\nMeu Portifólio!";
+  
+//       document.body.appendChild(alerta);
+  
+//       setTimeout(function() {
+//         alerta.remove();
+//       }, 3500); // Remove o alerta após 5 segundos
+//     }, 1000); // Exibe o alerta após 1,5 segundos
+//   }
+  
+//   // Chama a função ao carregar a página
+//   window.onload = function() {
+//     exibirAlerta();
+//   };
+  
